a0250bacb4251f475c0b193a521159c33154515a
KL Tah <kltah@gmail.com>
2017-06-06 14:05:18 +0800

    italy

--- italy/public/script.js
+++ italy/public/script.js
@@ -15,11 +15,11 @@ var map = new mapboxgl.Map({
   // style URL
   style: 'mapbox://styles/mapbox/streets-v9',
   // initial position in [long, lat] format
-  center: [-77.034084, 38.909671],
+  center: [ 12.393094,43.108102],
   //pitch: 80,
 //  bearing: 60,
   // initial zoom
-  zoom: 16
+  zoom: 6
 });
 var stores =       {
         "type": "FeatureCollection",
@@ -29,19 +29,20 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.034084142948,
-                38.909671288923
+                14.236688, 40.556101
               ]
             },
             "properties": {
-              "phoneFormatted": "(202) 234-7336",
-              "phone": "2022347336",
-              "address": "1471 P St NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at 15th St NW",
-              "postalCode": "20005",
-              "state": "D.C."
+              "phoneFormatted1": "+39 081 837 9671",
+              "phone1": "390818379671",
+              "phoneFormatted2": "",
+              "phone2": "",
+              "address": "Excelsior Parco, Via Marina Grande, 179",
+              "city": "Capri",
+              "country": "Italy",
+              "crossStreet": "",
+              "postalCode": "80076",
+              "state": "NA"
             }
           },
           {
@@ -49,19 +50,20 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.049766,
-                38.900772
+                14.260043, 40.849698
               ]
             },
             "properties": {
-              "phoneFormatted": "(202) 507-8357",
-              "phone": "2025078357",
-              "address": "2221 I St NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at 22nd St NW",
-              "postalCode": "20037",
-              "state": "D.C."
+              "phoneFormatted1": "+(39) 328 411 6579",
+              "phone1": "393284116579",
+              "phoneFormatted2": "+39 338 525 3216",
+              "phone2": "393385253216",
+              "address": "Vico Paparelle Al Pendino, 5",
+              "city": "Naples",
+              "country": "Italy",
+              "crossStreet": "",
+              "postalCode": "80138",
+              "state": "Campania"
             }
           },
           {
@@ -69,19 +71,20 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.043929,
-                38.910525
+                9.189243, 45.476997
               ]
             },
             "properties": {
-              "phoneFormatted": "(202) 387-9338",
-              "phone": "2023879338",
-              "address": "1512 Connecticut Ave NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at Dupont Circle",
-              "postalCode": "20036",
-              "state": "D.C."
+              "phoneFormatted1": "+39 338 471 1983",
+              "phone1": "393384711983",
+              "phoneFormatted2": "",
+              "phone2": "",
+              "address": "Via della Moscova, 27",
+              "city": "Milano",
+              "country": "Italy",
+              "crossStreet": "",
+              "postalCode": "20121",
+              "state": "Lombardia"
             }
           },
           {
@@ -89,19 +92,20 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.0672,
-                38.90516896
+                12.325527, 45.443876
               ]
             },
             "properties": {
-              "phoneFormatted": "(202) 337-9338",
-              "phone": "2023379338",
-              "address": "3333 M St NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at 34th St NW",
-              "postalCode": "20007",
-              "state": "D.C."
+              "phoneFormatted1": "+39 345 026 4156",
+              "phone1": "393450264156",
+              "phoneFormatted2": "",
+              "phone2": "",
+              "address": "Ponte delle Guglie 1304",
+              "city": "Venice",
+              "country": "Italy",
+              "crossStreet": "",
+              "postalCode": "30121",
+              "state": "Veneto"
             }
           },
           {
@@ -109,156 +113,23 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.002583742142,
-                38.887041080933
+                 12.489472, 41.894069
               ]
             },
             "properties": {
-              "phoneFormatted": "(202) 547-9338",
-              "phone": "2025479338",
-              "address": "221 Pennsylvania Ave SE",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "btwn 2nd & 3rd Sts. SE",
-              "postalCode": "20003",
-              "state": "D.C."
+              "phoneFormatted1": "+34 666 498 222",
+              "phone1": "34666498222",
+              "phoneFormatted2": "+34 93 221 36 27",
+              "phone2": "34932213627",
+              "address": "Via dell'Agnello",
+              "city": "Roma",
+              "country": "Italy",
+              "crossStreet": "",
+              "postalCode": "00184",
+              "state": "Lazio"
             }
           },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -76.933492720127,
-                38.99225245786
-              ]
-            },
-            "properties": {
-              "address": "8204 Baltimore Ave",
-              "city": "College Park",
-              "country": "United States",
-              "postalCode": "20740",
-              "state": "MD"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.097083330154,
-                38.980979
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(301) 654-7336",
-              "phone": "3016547336",
-              "address": "4831 Bethesda Ave",
-              "cc": "US",
-              "city": "Bethesda",
-              "country": "United States",
-              "postalCode": "20814",
-              "state": "MD"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.359425054188,
-                38.958058116661
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(571) 203-0082",
-              "phone": "5712030082",
-              "address": "11935 Democracy Dr",
-              "city": "Reston",
-              "country": "United States",
-              "crossStreet": "btw Explorer & Library",
-              "postalCode": "20190",
-              "state": "VA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.10853099823,
-                38.880100922392
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(703) 522-2016",
-              "phone": "7035222016",
-              "address": "4075 Wilson Blvd",
-              "city": "Arlington",
-              "country": "United States",
-              "crossStreet": "at N Randolph St.",
-              "postalCode": "22203",
-              "state": "VA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -75.28784,
-                40.008008
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(610) 642-9400",
-              "phone": "6106429400",
-              "address": "68 Coulter Ave",
-              "city": "Ardmore",
-              "country": "United States",
-              "postalCode": "19003",
-              "state": "PA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -75.20121216774,
-                39.954030175164
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(215) 386-1365",
-              "phone": "2153861365",
-              "address": "3925 Walnut St",
-              "city": "Philadelphia",
-              "country": "United States",
-              "postalCode": "19104",
-              "state": "PA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.043959498405,
-                38.903883387232
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(202) 331-3355",
-              "phone": "2023313355",
-              "address": "1901 L St. NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at 19th St",
-              "postalCode": "20036",
-              "state": "D.C."
-            }
-          }
+
         ]
       }
 
@@ -274,7 +145,7 @@ map.on('load', function(e) {
       data: stores
     },
     layout: {
-      'icon-image': 'restaurant-15',
+      'icon-image': 'lodging-15',
       'icon-allow-overlap': true,
     }
   });
